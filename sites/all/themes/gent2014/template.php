b65202bbb43a4f8fbc3b3cdef079dbbd12bab022
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2014-09-12 13:01:00 +0200

    Session detail page

--- sites/all/themes/gent2014/template.php
+++ sites/all/themes/gent2014/template.php
@@ -7,13 +7,13 @@
  * @param  string $hook      [description]
  */
 function gent2014_preprocess_field(&$variables, $hook) {
-  if ($variables['element']['#field_name'] == 'field_experience') {
+  if ($variables['element']['#field_name'] == 'field_experience' && $variables['element']['#view_mode'] == 'teaser') {
     $variables['classes_array'][] = 'experience-level';
     foreach ($variables['element']['#items'] as $item) {
       $variables['classes_array'][] = 'experience-level-' . $item['value'];
     }
   }
-  elseif ($variables['element']['#field_name'] == 'og_vocabulary' && $variables['element']['#bundle'] == 'session') {
+  elseif ($variables['element']['#field_name'] == 'og_vocabulary' && $variables['element']['#bundle'] == 'session' && $variables['element']['#view_mode'] == 'teaser') {
     foreach ($variables['element']['#items'] as $key => $item) {
       if (!empty($item['entity'])) {
         $colors = field_get_items('taxonomy_term', $item['entity'], 'field_track_color');
@@ -33,19 +33,4 @@ function gent2014_preprocess_field(&$variables, $hook) {
     $variables['classes_array'][] = 'schedule_item';
     $variables['classes_array'][] = 'schedule_' . check_plain($type[0]['value']);
   }
-
-
-  // elseif ($element['#field_name'] == 'field_session_track') {
-  //   foreach ($variables['items'] as $delta => $item) {
-  //     if (empty($item['#options']['entity'])) {
-  //       continue;
-  //     }
-  //     $colors = field_get_items('taxonomy_term', $item['#options']['entity'], 'field_track_color_code');
-  //     if (!empty($colors) && !empty($colors[0]['rgb'])) {
-  //       $rgb = $colors[0]['rgb'];
-  //       // $rgb = str_replace('#', '', $rgb);
-  //       $variables['item_attributes_array'][$delta]['style'] = 'background: ' . $rgb;
-  //     }
-  //   }
-  // }
 }
